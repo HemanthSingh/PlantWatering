53ff5771ca18086ab4e7ef5357b07116d6b22e03
Hemanth singh Jagadeeshwar <hemanthsingh.in@gmail.com>
2019-03-18 09:49:00 +0100

    MicroBit code updated

--- MicroBit.py
+++ MicroBit.py
@@ -6,9 +6,10 @@ REFRESH = 500
 def get_data():
     moisture = pin0.read_analog() #level of the tank 0 if empty
     level = pin1.read_analog()    #moisture level 0 to 1023 based on conductvity
-    print(moisture,level)                    #print to transmit this using UART
-    if level >100       #tank has water
+    #print(moisture,level)                    #print to transmit this using UART
+    if level >500:       #tank has water
         display.show(Image.HAPPY)
+        sleep(REFRESH)
         display.scroll(str(moisture))        #display on LED
         if moisture>500:
             pin2.write_digital(1)
@@ -17,6 +18,7 @@ def get_data():
 
     else:           #tank is empty
         display.show(Image.SAD)
+        sleep(REFRESH)
 
 def run():
     while True:
